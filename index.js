748550be0e23f3614a03ace3977a8be119b0fdc4
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-24 18:55:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 
 
 class Driver {
-  
+  constructor(name) {
+    this.id = ++driverId;
+    this.name = name;
+
+    store.drivers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId == this.id;
+    });
+  }
+
+  passengers() {
+    return this.trips().map(trip => {
+      return trip.passenger();
+    });
+  }
 }
+
 
 class Passenger {
   
