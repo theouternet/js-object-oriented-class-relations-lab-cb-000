cb0a6f6b116d44174a7f9b1a41155badc92ff8d8
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-24 19:02:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
  let store = { drivers: [], passengers: [], trips: [] }
 
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+
 class Driver {
   constructor(name) {
     this.id = ++driverId
